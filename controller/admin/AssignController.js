a6f26b3b9cde6b59a60cb593a4c312e20cfed877
Nishant-l <nishant3151998@gmail.com>
2021-11-06 12:09:29 +0530

    🧹 :code cleaning reduced reduncency in the code;

--- controller/admin/AssignController.js
+++ controller/admin/AssignController.js
@@ -13,11 +13,18 @@ module.exports.logout = (req,res) => {
 
 module.exports.assignForReviewForm = (req,res) => {
     Employee.findById(req.body.reveiwer,(err,reveiwer)=>{ //find the reveiver by id
-
+        if(!req.body.toBeReviewed instanceof Object){
+            req.body.toBeReviewed = [req.body.toBeReviewed];
+        }
         if(req.body.toBeReviewed instanceof Object){ //check if the toBeRevieved is a single value or js object
             for(r of req.body.toBeReviewed){ //if js object iterate over it
                 let count =0; 
                 for(k of reveiwer.toReview){ //iterate over all the to be reveived array to check if the id is alredy present
+                    if(k._id.toHexString() == r){ //if present increase the count
+                        count++;
+                    }
+                }
+                for(k of reveiwer.hadReviewed){ //check the employee is alredy revieved by same peer employee
                     if(k._id.toHexString() == r){ //if present increase the count
                         count++;
                     }
@@ -27,18 +34,6 @@ module.exports.assignForReviewForm = (req,res) => {
                     reveiwer.toReview.push(r); 
                 }
             }
-        }else{ //if toBeRevieved is a singlr value
-            let count =0; 
-                for(k of reveiwer.toReview){ //iterate over all the to be reveived array to check if the id is alredy present
-                    if(k.id == req.body.toBeReviewed){ //if present increase the count
-                        count++;
-                    }
-                }
-                // console.log(count);
-                if(count == 0){ //if the to_be_reveived id is not present in the array
-                    reveiwer.toReview.push(req.body.toBeReviewed); 
-                }
-
         }
         reveiwer.save(); //save the changes in database
         // console.log(reveiwer.toReview)
