b136f1964bb0abd5c0d4cf6cdd43551b3e691234
Nishant-l <nishant3151998@gmail.com>
2021-11-07 09:05:29 +0530

    ✍️ :Employmanagement update employee name and email;

--- controller/admin/employeeManagementController.js
+++ controller/admin/employeeManagementController.js
@@ -1,3 +1,34 @@
-module.exports.demo = (req,res)=>{
-    res.render('employeeManagement');
+const Employee = require('../../model/employe');
+
+module.exports.demo = (req,res)=>{ // to display the employeeManagement page
+    Employee.find({},(err,AllEmployee) => {
+        res.render('employeeManagement',{AllEmployee:AllEmployee});
+    })
 };
+
+module.exports.updateEmployeeProfile = (req,res) => { //controller to display a specific employees page to enable editing employees info
+    Employee.findById(req.params.empid)
+            .populate({
+                path:'myReview',
+                populate:{
+                    path:'reviewedBy'
+                }
+            })
+            .exec((err,emp)=>{
+                // console.log(req.params);
+                res.render('updateEmpInfo',{empInfo:emp});
+                // console.log(emp);
+            })
+}
+
+module.exports.updateEmpInfo_form = (req,res)=>{ // controller to update employee name and email which are input through form
+    Employee.findById(req.params.id,(err,employee)=>{
+        if(employee.name != req.body.name || employee.email != req.body.email){
+            employee.name = req.body.name;
+            employee.email = req.body.email;
+            employee.save();
+            res.redirect('back');
+            console.log(employee);
+        }
+    })
+}
--- routs/admin/employeeManagement.js
+++ routs/admin/employeeManagement.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const { isFunctionOrConstructorTypeNode } = require('typescript');
 const router = express.Router();
 const empMngController = require('../../controller/admin/employeeManagementController');
 
-router.get('/',empMngController.demo);
+router.get('/viewAllEmployee',empMngController.demo);
+router.get('/Update-emloyee-profile/:empid',empMngController.updateEmployeeProfile);
+
+router.post('/update-form/:id',empMngController.updateEmpInfo_form);
 
 module.exports = router;
